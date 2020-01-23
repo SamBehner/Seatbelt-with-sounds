f5ad529cab3de36b3961091e6e06c3dd0d277555
TehRamsus <50022415+TehRamsus@users.noreply.github.com>
2020-01-23 22:09:40 +0200

    v.1.1.3 Fixed some issues, check desc
    - Deleted animations
    - Deleted pNotify, and added gta own "SetNotificationTextEntry" message
    - (optional) removed loop "bling" sound

--- seatbelt/main.lua
+++ seatbelt/main.lua
@@ -1,114 +1,111 @@
-local isUiOpen		= false 
-local speedBuffer	= {}
-local velBuffer		= {}
-local beltOn		= false
-local wasInCar		= false
+local isUiOpen = false 
+local speedBuffer  = {}
+local velBuffer    = {}
+local beltOn       = false
+local wasInCar     = false
+
+function notify(string)
+  SetNotificationTextEntry("STRING")
+  AddTextComponentString(string)
+  DrawNotification(false, true)
+end
 
 IsCar = function(veh)
-	local vc = GetVehicleClass(veh)
+        local vc = GetVehicleClass(veh)
         return (vc >= 0 and vc <= 7) or (vc >= 9 and vc <= 12) or (vc >= 17 and vc <= 20)
-end 
+        end 
 
 Fwv = function (entity)
-	local hr = GetEntityHeading(entity) + 90.0
+        local hr = GetEntityHeading(entity) + 90.0
         if hr < 0.0 then hr = 360.0 + hr end
         hr = hr * 0.0174533
         return { x = math.cos(hr) * 2.0, y = math.sin(hr) * 2.0 }
-end
-
-Citizen.CreateThread(function()
+      end
+ 
+	Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(0)
+	Citizen.Wait(0)
+  
+    local ped = GetPlayerPed(-1)
+    local car = GetVehiclePedIsIn(ped)
+    
+    if car ~= 0 and (wasInCar or IsCar(car)) then
+      wasInCar = true
+             if isUiOpen == false and not IsPlayerDead(PlayerId()) then
+                SendNUIMessage({
+            	   displayWindow = 'true'
+            	   })
+                isUiOpen = true
+            end
 
-		local ped = GetPlayerPed(-1)
-		local car = GetVehiclePedIsIn(ped)
+       if beltOn then 
+	  DisableControlAction(0, 75, true)  -- Disable exit vehicle when stop
+	  DisableControlAction(27, 75, true) -- Disable exit vehicle when Driving
+	  end
 
-		if car ~= 0 and (wasInCar or IsCar(car)) and not IsPauseMenuActive() then
-			wasInCar = true				
-			if isUiOpen == false and not IsPlayerDead(PlayerId()) then
-				SendNUIMessage({
-					displayWindow = 'true'
-				})
-                		isUiOpen = true
-			end
-
-			if beltOn then 
-				DisableControlAction(0, 75, true)  -- Disable exit vehicle when stop
-				DisableControlAction(27, 75, true) -- Disable exit vehicle when Driving
-			end
-
-			speedBuffer[2] = speedBuffer[1]
-			speedBuffer[1] = GetEntitySpeed(car)
+      speedBuffer[2] = speedBuffer[1]
+      speedBuffer[1] = GetEntitySpeed(car)
 
 
-			if speedBuffer[2] ~= nil 
-			  and not beltOn
-			  and GetEntitySpeedVector(car, true).y > 1.0  
-			  and speedBuffer[1] > 19.25 
-			  and (speedBuffer[2] - speedBuffer[1]) > (speedBuffer[1] * 0.255) then
-
-				local co = GetEntityCoords(ped)
-				local fw = Fwv(ped)
-				SetEntityCoords(ped, co.x + fw.x, co.y + fw.y, co.z - 0.47, true, true, true)
-				SetEntityVelocity(ped, velBuffer[2].x, velBuffer[2].y, velBuffer[2].z)
-				Citizen.Wait(1)
-				SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
-			end
-
-			velBuffer[2] = velBuffer[1]
-			velBuffer[1] = GetEntityVelocity(car)
-
-			if IsControlJustReleased(0, 344) and GetLastInputMethod(0) then
-				TriggerEvent("FakeRevive")
-				RegisterNetEvent("FakeRevive")
-				AddEventHandler("FakeRevive", function(inputText) 
-				RequestAnimDict("oddjobs@taxi@cyi")
-				TaskPlayAnim(GetPlayerPed(-1),"oddjobs@taxi@cyi", "std_hand_off_ps_passenger", 8.0, -8.0, -1, 0, 0, false, false, false)
-			end)
-
-        		beltOn = not beltOn 
-			if beltOn then 
-				TriggerEvent("pNotify:SendNotification", {text = "Turvavyö käytössä", type = "success", timeout = 1400, layout = "centerLeft"})
-
-				--- Täältä voit muuttaa ääntä, kun turvavyö laitetaan päälle -- Tässä ----
-    				---- Here you can change sounds, when you put seatbelt on ----- Here -----
-				---- Hier kannst du den Sound beim Anschnallen ändern --------- Hier -----
-				TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 0.9, 'buckle', 0.9)
-
-				SendNUIMessage({
-					displayWindow = 'false'
-				})
-				isUiOpen = true
-
-			else 
-				TriggerEvent("pNotify:SendNotification", {text = "Turvavyö otettu", type = "error", timeout = 1400, layout = "centerLeft"})
-
-				--- Täältä voit muuttaa ääntä, kun turvavyö otetaan pois ------ Tässä ----
-				---- Here you can change sounds, when you take seatbelt off --- Here -----
-				---- Hier kannst du den Sound beim ABschnallen ändenr --------- Hier -----
-				TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 0.9, 'unbuckle', 0.9)
-
-				SendNUIMessage({
-					displayWindow = 'true'
-				})
-				isUiOpen = true
-			end
-
-		end
       
-		elseif wasInCar then
-			wasInCar = false
-			beltOn = false
-			speedBuffer[1], speedBuffer[2] = 0.0, 0.0
-			if isUiOpen == true and not IsPlayerDead(PlayerId()) then
-				SendNUIMessage({
-					displayWindow = 'false'
-				})
-				isUiOpen = false 
-			end
-		end
+      if speedBuffer[2] ~= nil 
+         and not beltOn
+         and GetEntitySpeedVector(car, true).y > 1.0  
+         and speedBuffer[1] > 19.25 
+         and (speedBuffer[2] - speedBuffer[1]) > (speedBuffer[1] * 0.255) then
+         
+        local co = GetEntityCoords(ped)
+        local fw = Fwv(ped)
+        SetEntityCoords(ped, co.x + fw.x, co.y + fw.y, co.z - 0.47, true, true, true)
+        SetEntityVelocity(ped, velBuffer[2].x, velBuffer[2].y, velBuffer[2].z)
+        Citizen.Wait(1)
+        SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
+      end
+        
+      velBuffer[2] = velBuffer[1]
+      velBuffer[1] = GetEntityVelocity(car)
+        
+      if IsControlJustReleased(0, 311) and GetLastInputMethod(0) then
+        beltOn = not beltOn 
+        if beltOn then
+        Citizen.Wait(1)
 
-	end
+		  	--- Täältä voit muuttaa ääntä, kun turvavyö laitetaan päälle -- Tässä ----
+    		---- Here you can change sounds, when you put seatbelt on ----- Here -----
+		  TriggerServerEvent('InteractSound_SV:PlayOnSource', 'buckle', 0.9)
+      Citizen.Wait(2500)
+      notify('~y~Seatbelt~s~: ~g~connected~s~')
+			
+		  SendNUIMessage({
+		    displayWindow = 'false'
+		    })
+		  isUiOpen = true 
+		else 
+		  notify('~y~Seatbelt~s~: ~r~disconnected~s~')
+		      	--- Täältä voit muuttaa ääntä, kun turvavyö otetaan pois -- Tässä ------
+    			---- Here you can change sounds, when you take seatbelt off - Here -----
+		  TriggerServerEvent('InteractSound_SV:PlayOnSource', 'unbuckle', 0.9)
+
+		  SendNUIMessage({
+		     displayWindow = 'true'
+		     })
+		  isUiOpen = true  
+		end
+      end
+      
+    elseif wasInCar then
+      wasInCar = false
+      beltOn = false
+      speedBuffer[1], speedBuffer[2] = 0.0, 0.0
+             if isUiOpen == true and not IsPlayerDead(PlayerId()) then
+                SendNUIMessage({
+            	   displayWindow = 'false'
+            	   })
+                isUiOpen = false 
+            end
+    end
+    
+  end
 end)
 
 Citizen.CreateThread(function()
@@ -123,6 +120,7 @@ Citizen.CreateThread(function()
 	end
 end)
 
+--[[
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
@@ -136,3 +134,4 @@ Citizen.CreateThread(function()
 		end
 	end
 end)
+--]]
